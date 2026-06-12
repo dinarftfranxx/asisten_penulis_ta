36a16c55bf6738cf62543c5655078e036df6ffee
dinarftfranxx <dinaryourbae351@gmail.com>
2026-06-12 21:15:05 +0700

    fix vercel pip install error

--- build.sh
+++ build.sh
@@ -2,7 +2,8 @@
 # exit on error
 set -o errexit
 
-pip install -r requirements.txt
+# pip install -r requirements.txt
+pip install -r requirements.txt --break-system-packages
 
 python manage.py collectstatic --no-input
 # python manage.py migrate  # Dinonaktifkan untuk Vercel (Migrasi dilakukan via lokal)
